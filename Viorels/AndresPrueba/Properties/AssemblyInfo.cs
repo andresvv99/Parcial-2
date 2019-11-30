603aaae99c11eb1af5a60b74eb7740e27a712794
andresvv99 <andres.viorels99@gmail.com>
2019-11-30 10:31:31 -0400

    Solucion finalizada

---
+++ Viorels/AndresPrueba/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("AndresPrueba")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("AndresPrueba")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("65cb044e-8223-4eb6-b4f9-bb48d608a9cd")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Viorels/AndresPrueba/UnitTest1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using Andres;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AndresPrueba
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void PruebaSave()
+        {
+            //Organizar
+            Person account = new Person("Arrazola Juanpa", 5000);
+            double valoresperado = 2000;
+
+
+            //Actuar
+            account.Save(3000);
+            double valorobtenido = account.SaveMoney;
+
+            // Afirmar
+            Assert.AreEqual(valoresperado, valorobtenido);
+
+        }
+        [TestMethod]
+
+
+        public void PruebaSpend()
+        {
+            //Organizar
+            Person account = new Person("Arrazola Juanpa", 1000);
+            double valoresperado = 5000;
+
+
+            //Actuar
+            account.Spend(4000);
+            double valorobtenido = account.SaveMoney;
+
+            // Afirmar
+            Assert.AreEqual(valoresperado, valorobtenido);
+
+        }
+        [TestMethod]
+
+        public void PruebaChangeName()
+        {
+            //Organizar
+            Person account = new Person("David Gutierrez",0);
+            string valoresperado = "David Gutierrez";
+
+
+            //Actuar
+            account.ChangeName("Julio Mercado");
+            string valorobtenido = account.Name;
+
+            // Afirmar
+            Assert.AreEqual(valoresperado, valorobtenido);
+
+        }
+    }
+}
